abefd3f0f6f70e38a5766a52908b86f2d5f40b62
DHONCRISLEY LINS DE OLIVEIRA <dhoncrisley@hotmail.com>
2020-10-16 21:20:47 -0300

    remove types and add getLocale to provider

--- src/provider.js
+++ src/provider.js
@@ -1,56 +1,51 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { translateKey, translate } from './utils'
-import { TranslateProvider } from './context'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { translateKey, translate } from './utils';
+import { TranslateProvider } from './context';
 
+/**
+ * Provider
+ */
 class IntlProvider extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     if (!props.translations || !props.locale) {
-      let namePart = this.constructor.displayName
-        ? ' of ' + this.constructor.displayName
-        : ''
-      throw new Error(
-        'Could not find translations or locale on this.props ' + namePart
-      )
+      let namePart = this.constructor.displayName ? ' of ' + this.constructor.displayName : '';
+      throw new Error('Could not find translations or locale on this.props ' + namePart);
     }
   }
 
   static propTypes = {
-    translations: PropTypes.object
-  }
+    translations: PropTypes.object,
+  };
   static defaultProps = {
-    translations: {}
-  }
+    translations: {},
+  };
   translate = (key, placeholders, isHTML, options = {}) => {
     /**
      * Accept user defined translate
      */
-    const translateFn = this.props.translate || translateKey
-    return translate(translateFn,
-        this.props.translations,
-        this.props.locale,
-        key,
-        placeholders,
-        isHTML,
-        options);
-  }
+    const translateFn = this.props.translate || translateKey;
+    return translate(translateFn, this.props.translations, this.props.locale, key, placeholders, isHTML, options);
+  };
+  getLocale = () => this.props.locale;
+  getLocaleName = () => this.props.localeName;
+
   render() {
     return (
-      <TranslateProvider value={this.translate}>
+      <TranslateProvider
+        value={{ translate: this.translate, getLocale: this.getLocale.bind(this), getLocaleName: this.getLocaleName.bind(this) }}
+      >
         {this.props.children}
       </TranslateProvider>
-    )
+    );
   }
 }
 
-function mapPropsToState(state) {
-  const { Intl } = state
-  return {
-    ...Intl,
-    key: Intl.locale
-  }
-}
+const mapPropsToState = (state) => ({
+  ...state.Intl,
+  key: state.Intl.locale,
+});
 
-export default connect(mapPropsToState)(IntlProvider)
+export default connect(mapPropsToState)(IntlProvider);
